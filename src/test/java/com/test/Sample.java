7395a169b7125b1e630e730dfcdcbe4ff177f780
ManiMech12 <mani.mechanicalengineer12@gmail.com>
2021-04-28 14:33:34 +0530

    class 2 work

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+
+	public void tc1() {
+		System.out.println("test1");
+	}
+
+	@Test
+	public void tc2() {
+		System.out.println("test2");
+	}
+
+	@Test
+
+	public void tc3() {
+		System.out.println("test3");
+	}
+
+	@Test
+	public void tc4() {
+		System.out.println("test4");
+	}
+
+	@Test
+
+	public void tc5() {
+		System.out.println("test5");
+	}
+
+}
